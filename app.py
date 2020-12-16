67b97fc88ea678b9168de2630398ef540e8e38c7
Richiefinegan11 <richiefinegan1990@gmail.com>
2020-12-16 18:58:36 +0000

    Debug set to false

--- app.py
+++ app.py
@@ -202,4 +202,4 @@ def delete_account_confirmed():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
